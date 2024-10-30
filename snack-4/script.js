9cb8b7ed60f098c1665debb637b952e3daf92c40
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-30 15:36:55 +0100

    Snack 04

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,9 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authors = posts.map(({ author }) => author)
+console.log(authors)
+
+const autori = posts.map((el) => el.author)
+console.log(autori)
